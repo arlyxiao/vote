063f6849517106f29fbaaa153e0330a10331452c
luffycn <kingla_pei@163.com>
2012-03-20 15:29:34 +0800

    Change kind data type from votes table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120320061612) do
+ActiveRecord::Schema.define(:version => 20120320072704) do
 
   create_table "answer_votes", :force => true do |t|
     t.integer  "user_id"
@@ -174,7 +174,7 @@ ActiveRecord::Schema.define(:version => 20120320061612) do
     t.integer  "select_limit"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "kind"
+    t.string   "kind",         :default => "TEXT"
   end
 
 end
